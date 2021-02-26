e1a4c50f6d8aee445797cb7613b757db3567cecd
volunux <37976953+volunux@users.noreply.github.com>
2021-02-26 09:29:31 -0800

    Day3

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', (req, res, next) => {
 
-	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
 
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type,Etag, Accept');
 	res.setHeader('Access-Control-Allow-Methods', 'GET , POST , OPTIONS , DELETE , PUT , PATCH');
